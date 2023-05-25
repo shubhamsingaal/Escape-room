967263eb5098337eacd919b03c53abe6914f8a40
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2023-05-25 06:56:03 +0000

    tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // The function format is required to prevent the input files (in this case `.gitignore`) from
   // being appended to the `ignore-sync` and `git` commands
   ".gitignore": () => [
-    // "ignore-sync .eslintignore-sync .prettierignore-sync",
+    "ignore-sync .eslintignore-sync .prettierignore-sync",
     "git add .eslintignore .prettierignore",
   ],
 };
