51e2e3660da944b357031b12b20eed01ed1f3087
Shubham Kumar <shubhamkraj200@gmail.com>
2023-05-26 12:03:38 +0530

    a

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="https://www.facebook.com/istenitdgp/" target="blank"><img src={navIcon2} alt="" /></a>
               <a href="https://www.instagram.com/istenitdgp/?hl=en" target="blank"><img src={navIcon3} alt="" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2023. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
