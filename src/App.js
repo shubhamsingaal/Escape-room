de5a7b90afbb8944f7038242e83220d4470e38ff
Shubham Kumar <shubhamkraj200@gmail.com>
2023-05-26 11:20:56 +0530

    landing, Rules and about created

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import logo from '.';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Rules } from "./components/Rules";
+import { About } from "./components/About";
 
+import { Footer } from "./components/Footer";
 
-/** import all components */
-import Username from './components/Username';
-import Password from './components/Password';
-import Register from './components/Register';
-import Profile from './components/Profile';
-import Recovery from './components/Recovery';
-import Reset from './components/Reset';
-import PageNotFound from './components/PageNotFound';
-
-
-/** auth middleware */
-import { AuthorizeUser, ProtectRoute } from './middleware/auth'
-
-/** root routes */
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Username></Username>
-  },
-  {
-    path: '/register',
-    element: <Register></Register>
-  },
-  {
-    path: '/password',
-    element: <ProtectRoute><Password /></ProtectRoute>
-  },
-  {
-    path: '/profile',
-    element: <AuthorizeUser><Profile /></AuthorizeUser>
-  },
-  {
-    path: '/recovery',
-    element: <Recovery></Recovery>
-  },
-  {
-    path: '/reset',
-    element: <Reset></Reset>
-  },
-  {
-    path: '*',
-    element: <PageNotFound></PageNotFound>
-  },
-])
-
-export default function App() {
+function App() {
   return (
-    <main>
-      <RouterProvider router={router}></RouterProvider>
-    </main>
-  )
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Rules />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
+export default App;
