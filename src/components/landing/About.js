02d530d9ac9ee06d5a369903a3df9d75287a50fe
Shubham Kumar <shubhamkraj200@gmail.com>
2023-06-24 10:31:45 +0530

    updated about

--- src/components/landing/About.js
+++ src/components/landing/About.js
@@ -71,41 +71,7 @@ export const  About = () => {
                     
                 
                   </p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Escape room is a part of the panaroma event </p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                        <p>Escape room is a part of the panaroma event</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
+        
               </div>}
             </TrackVisibility>
           </Col>
